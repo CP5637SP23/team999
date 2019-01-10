e7b06a87c296eea8c671e07cc78875b72668f3a5
Manish Maharjan <manish.maharjan@my.jcu.edu.au>
2019-01-11 01:42:45 +1000

    Navigation Menu and Custom Logo Created

---
+++ themes/team999/js/scripts.js
@@ -0,0 +1,22 @@
+//Custom Slider
+jQuery(document).ready(function($){
+	'use strict';
+
+//alert("Hello! I am an alert box!!");
+	$(window).load(function(){
+		$('.flexslider').flexslider({
+			animation:"slide"
+		});
+	});
+}(jQuery));
+
+// (function ($, root, undefined) {
+//
+// 	$(function () {
+//
+// 		'use strict';
+//
+//
+// 	});
+//
+// })(jQuery, this);
